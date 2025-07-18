fba5ff3e162eedc8b84c30d98019d7931d1b861f
ulluk <hofutop@gmail.com>
2025-07-18 20:41:55 +0600

    feat: added new model with ModelForm

--- posts/forms.py
+++ posts/forms.py
@@ -1,21 +1,29 @@
 from django import forms
+from .models import Post
 
-class PostForm(forms.Form):
-    image = forms.ImageField(required=False)
-    title = forms.CharField(required=True, max_length=256)
-    content = forms.CharField(required=True, max_length=456)
+# class PostForm(forms.Form):
+#     image = forms.ImageField(required=False)
+#     title = forms.CharField(required=True, max_length=256)
+#     content = forms.CharField(required=True, max_length=456)
 
-    def clean_title(self):
-        cleaned_data = super().clean()
-        title = cleaned_data.get("title")
-        if title and title.lower() == 'python':
-            raise forms.ValidationError('Title cannot be Python')
-        return title
+#     def clean_title(self):
+#         cleaned_data = super().clean()
+#         title = cleaned_data.get("title")
+#         if title and title.lower() == 'python':
+#             raise forms.ValidationError('Title cannot be Python')
+#         return title
     
-    def clean(self):
-        cleaned_data = super().clean()
-        title = cleaned_data.get("title")
-        content = cleaned_data.get('content')
-        if title and title.lower() == content.lower():
-            raise forms.ValidationError('Title and content cannot be same')
-        return cleaned_data
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         title = cleaned_data.get("title")
+#         content = cleaned_data.get('content')
+#         if title and title.lower() == content.lower():
+#             raise forms.ValidationError('Title and content cannot be same')
+#         return cleaned_data
+    
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'image']
--- posts/views.py
+++ posts/views.py
@@ -18,17 +18,31 @@ def post_detail_view(request, post_id):
     return render(request, 'posts/post_detail.html', context = {'post': post})
 
 def post_create_view(request):
-    if request.method == 'GET':
-        form = PostForm()
-        return render(request, 'posts/post_create.html', context={"form":form})
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                post = form.save()
+                return redirect(f'/posts/{post.id}')
+        else:
+            form = PostForm()
     
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if not form.is_valid():
-            return render(request, 'posts/post_create.html', context={"form":form})
+        return render(request, 'posts/post_create.html', {'form': form})
+# def post_create_view(request):
+#     if request.method == 'GET':
+#         form = PostForm()
+#         return render(request, 'posts/post_create.html', context={"form":form})
+    
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES)
+#         if not form.is_valid():
+#             return render(request, 'posts/post_create.html', context={"form":form})
 
-        title = form.cleaned_data.get("title")
-        content = form.cleaned_data.get("content")
-        image = form.cleaned_data.get("image")
-        post = Post.objects.create(title = title, content = content, image = image)
-        return redirect(f'/posts/{post.id}')
+#         title = form.cleaned_data.get("title")
+#         content = form.cleaned_data.get("content")
+#         image = form.cleaned_data.get("image")
+#         post = Post.objects.create(title = title, content = content, image = image)
+#         return redirect(f'/posts/{post.id}')
+
+
+
+    
